2a13db5b4a74ef1beaa003d3bd4c6123eae4bf98
paula7026 <108918184+paula7026@users.noreply.github.com>
2022-07-10 22:58:03 +0800

    Add files via upload

---
+++ 6-4.js
@@ -0,0 +1,17 @@
+var user=new Object();
+user.name="John";
+user.fname="Mike";
+user.name="Peter";
+delete user.name;
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+    // write your code here...
+
+var num=0;
+num=fruit.apple+fruit.peach+fruit.pear;
+console.log(num);
+
